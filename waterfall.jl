03567e05af40123084af87bac7e7835b1dd7525b
Varnerlab <jdv27@cornell.edu>
2022-03-30 06:24:34 -0400

    small changes to waterfall
    some small changes

--- waterfall.jl
+++ waterfall.jl
@@ -2,7 +2,7 @@
 include("Include.jl")
 
 # how many samples?
-number_of_samples = 25
+number_of_samples = 500
 number_of_parameters = 12
 ensemble_archive = zeros(number_of_parameters+1,1); # first row is the fitness 
 
@@ -35,17 +35,19 @@ for i ∈ 1:number_of_samples
 
     # dump parameters to disk (just in case) -
     path_to_synthetic_ensemble = joinpath(_PATH_TO_TMP, "PSET-Synthetic-1k-P$(i).csv")
-    CSV.write(path_to_synthetic_ensemble, Tables.table(ensemble_archive))
+    CSV.write(path_to_synthetic_ensemble, Tables.table(ensemble_archive); header=["parameters"])
     
     # dump output to disk -
     data_output = [Y Yₘ]
+    data_output_header = ["actual", "simulated"]
     path_to_synthetic_patient_otuput = joinpath(_PATH_TO_TMP, "OUT-Synthetic-1k-P$(i).csv")
-    CSV.write(path_to_synthetic_patient_otuput, Tables.table(data_output))
+    CSV.write(path_to_synthetic_patient_otuput, Tables.table(data_output); header = data_output_header)
 
     # dump state to disk -
-    data_state = vcat(reshape(T,1,length(T)), Xₘ)
+    data_state = transpose(vcat(reshape(T,1,length(T)), Xₘ))
+    data_state_header = ["T", "FII", "FVII", "FV", "FX", "FVIII", "FIX", "FXI", "FXII", "FIIa", "FVIIa", "FVa", "FXa", "FVIIIa", "FIXa", "FXIa", "FXIIa", "FIIa_inactive"]
     path_to_synthetic_patient_simulation = joinpath(_PATH_TO_TMP, "SIM-Synthetic-1k-P$(i).csv")
-    CSV.write(path_to_synthetic_patient_simulation, Tables.table(data_state))
+    CSV.write(path_to_synthetic_patient_simulation, Tables.table(data_state); header=data_state_header)
 
     # update/clean up for the next patient -
     global p_previous = p
